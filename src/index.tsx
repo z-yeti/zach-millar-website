cd4b02bed51c9c94a1fde836ddec962ca377c066
Zach Millar <zach.millar@gmail.com>
2022-11-12 17:08:26 -0800

    - add styled-components - add font from adobe cdn - add GlobalStyles

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './containers/App'
 import reportWebVitals from './reportWebVitals'
+import { GlobalStyles } from './styles/GlobalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 )
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    box-sizing:border-box;
+    font-family: niveau-grotesk-small-caps,sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+  #root {
+    margin:0 auto;
+  }
+`
